ad8976eac0613dc62fccef711f5f7d67494ce946
Martin Fenner <martin.fenner@datacite.org>
2016-11-01 22:07:09 +0100

    prepare install of handle software

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
 default['java']['install_flavor'] = "oracle"
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['java']['jdk_version'] = "8"
+
+default['handle']['dir'] = "/hs"
+default['handle']['server_name'] = "svr_1"
+default['handle']['version'] = "8.1.1"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,44 @@
 include_recipe 'java'
+
+# root directory for handle server
+directory node['handle']['dir'] do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  recursive true
+  action :create
+end
+
+# download source code
+tarball = "hnr-source/hsj-#{node['handle']['version']}.tar.gz"
+download_url = "http://www.handle.net/#{tarball}"
+
+remote_file "#{node['handle']['dir']}/#{tarball}" do
+  source download_url
+  action :create_if_missing
+  mode '0644'
+end
+
+# uncompress
+execute "tar" do
+  user "root"
+  group "root"
+  cwd node['handle']['dir']
+  action :run
+  command "tar xvzf hnr-source/hsj-#{node['handle']['version']}.tar.gz"
+  not_if{ ::File.directory?("#{node['handle']['dir']}/hsj-#{node['handle']['version']}")
+end
+
+# install directory for handle server
+directory "#{node['handle']['dir']}/#{node['handle']['server_name']}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+execute "bin/hdl-setup-server #{node['handle']['dir']}/#{node['handle']['server_name']}" do
+  cwd "#{node['handle']['dir']}/hsj-#{node['handle']['version']}"
+  action :run
+end
+
