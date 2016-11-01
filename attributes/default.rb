127c38d44d000a266a869744f78b9e7e0989d542
Martin Fenner <martin.fenner@datacite.org>
2016-11-01 18:43:32 +0100

    use correct attribute setting

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-node['java']['install_flavor'] = "oracle"
-node['java']['oracle']['accept_oracle_download_terms'] = true
-node['java']['jdk_version'] = "8"
+default['java']['install_flavor'] = "oracle"
+default['java']['oracle']['accept_oracle_download_terms'] = true
+default['java']['jdk_version'] = "8"
