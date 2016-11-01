8ffa2718aeecac06b1f607dbd9e83819b96453e3
Martin Fenner <martin.fenner@datacite.org>
2016-11-01 18:32:46 +0100

    initial version

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name              "handle"
+maintainer        "Martin Fenner"
+maintainer_email  "mfenner@datacite.org"
+license           "Apache 2.0"
+description       "Configures handle server"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0"
+
+# opscode cookbooks
+depends           'apt'
+depends           'java', '~> 1.42.0'
+
+%w{ ubuntu }.each do |platform|
+  supports platform
+end
